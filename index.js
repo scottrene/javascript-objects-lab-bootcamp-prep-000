cd05786297e1ceb1632aac1bbb955ab592d400c0
michele lafleur <mlafleurr@gmail.com>
2018-08-09 22:26:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue (recipes, key, value) {
-  recipes = Object.assign(recipes, {key:value});
+  recipes = Object.assign(recipes, {key : value});
   return recipes;
 }
